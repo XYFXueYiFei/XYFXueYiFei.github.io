d672754e762790855cdb40f640571f8be3ff1c65
XYFXueYiFei <xueyifei@xueyifei.cn>
2025-08-07 04:16:32 +0800

    Site updated: 2025-08-07 04:16:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/hello-world/","2025/08/07/测试博文/"];function toRandomPost(){
+var posts=["2025/08/07/测试博文/","2025/08/07/娴嬭瘯浜斾釜瀛/","2025/08/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
