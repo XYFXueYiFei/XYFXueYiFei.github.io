ecd1d0331a00b7534e491d18b728fc4d04e4e100
XYFXueYiFei <xueyifei@xueyifei.cn>
2025-08-07 18:58:28 +0800

    Site updated: 2025-08-07 18:58:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/我说这个是测试你信吗/","2025/08/07/hello-world/","2025/08/07/测试博文/"];function toRandomPost(){
+var posts=["2025/08/07/第一篇博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
